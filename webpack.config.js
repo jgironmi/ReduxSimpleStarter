a678198b67b6159e64ba7539db21491c032add8f
Jose Gabriel Giron Hernandez <joseg@MacBook-Pro-de-Jose.local>
2016-05-20 16:48:38 -0600

    Chapter 26

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  devTools: 'eval-source-map',
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
